c2128495feeb3ac91b83344be7c1648eae53c01d
emon5122 <emon5122@gmail.com>
2023-06-09 00:34:40 +0600

    just basic ts -final version

--- app/(clerk)/user/page.tsx
+++
@@ -1,5 +0,0 @@
-import { UserProfile } from "@clerk/nextjs";
-const Profile = () => {
-    return <UserProfile />;
-};
-export default Profile;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import "./globals.css";
 import { Nunito } from "next/font/google";
-import { ClerkProvider } from "@clerk/nextjs";
 import { ReactNode } from "react";
 import Provider from "./provider";
 import { Metadata } from "next";
@@ -8,18 +7,16 @@ import { Metadata } from "next";
 const nunito = Nunito({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "Advanced Blog",
+    title: "Basic TS",
     description: "Hobby Project",
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
     return (
-        <ClerkProvider>
-            <html lang="en">
-                <body className={nunito.className}>
-                    <Provider>{children}</Provider>
-                </body>
-            </html>
-        </ClerkProvider>
+        <html lang="en">
+            <body className={nunito.className}>
+                <Provider>{children}</Provider>
+            </body>
+        </html>
     );
 }
